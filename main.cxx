88b8261485e75652e7f73342fb26831e46f1d992
kongdehua <kongdehua>
2015-12-02 00:05:45 +0800

    first

---
+++ main.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/** Main function */
+int main(int argc, char* argv[])
+{
+  double test = 2.3;
+  int t = test; // Warning
+  std::cout << "Running Test1" << std::endl;
+  return 0;
+}
